23773006b7383bd2f5f911e0bdb306f988ce660a
Bryn Webb <brynellenwebb@gmail.com>
2024-04-17 20:33:52 -0600

    added new photos, theme, and updates

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+function addDarkMode() {
+    var link = document.createElement("link");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    link.href = "css/darkMode.css";
+    link.id = "darkMode";
+
+    document.head.appendChild(link);
+
+    var dark = document.getElementById("dark");
+    var light = document.getElementById("light");
+
+    dark.style.display = "none";
+    light.style.display = "block";
+}
+
+function removeDarkMode() {
+    var head = document.head;
+    var link = document.getElementById("darkMode");
+
+    var dark = document.getElementById("dark");
+    var light = document.getElementById("light");
+
+    head.removeChild(link);
+    light.style.display = "none";
+    dark.style.display = "block";
+}
+
 function homeButton() {
     var h = document.getElementById("home");
     h.style.display = "flex";
